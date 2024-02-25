26323faba79ea4177d112f4b2ab03ef0352e99b5
Subham Singh <subham.reek@gmail.com>
2024-02-26 00:39:29 +0100

    Addition of the Run() method in the SpatialPatternLearning.cs
    Addition of

--- source/MySEProjectSample/MyProjectSample/SpatialPatternLearning.cs
+++ source/MySEProjectSample/MyProjectSample/SpatialPatternLearning.cs
@@ -12,12 +12,80 @@ using System.Reflection.Metadata.Ecma335;
 
 namespace NeoCortexApiSample
 {
+
     /// <summary>
     /// Implements an experiment that demonstrates how to learn spatial patterns.
     /// SP will learn every presented input in multiple iterations.
     /// </summary>
     public class SpatialPatternLearning
     {
+        public void Run()
+        {
+            Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(SpatialPatternLearning)}");
+
+            // Used as a boosting parameters
+            // that ensure homeostatic plasticity effect.
+            double minOctOverlapCycles = 1.0;
+            double maxBoost = 5.0;
+
+            // We will use 200 bits to represent an input vector (pattern).
+            int inputBits = 200;
+
+            // We will build a slice of the cortex with the given number of mini-columns
+            int numColumns = 1024;
+
+            //
+            // This is a set of configuration parameters used in the experiment.
+            HtmConfig cfg = new HtmConfig(new int[] { inputBits }, new int[] { numColumns })
+            {
+                CellsPerColumn = 10,
+                MaxBoost = maxBoost,
+                DutyCyclePeriod = 100,
+                MinPctOverlapDutyCycles = minOctOverlapCycles,
+
+                GlobalInhibition = false,
+                NumActiveColumnsPerInhArea = 0.02 * numColumns,
+                PotentialRadius = (int)(0.15 * inputBits),
+                LocalAreaDensity = -1,
+                ActivationThreshold = 10,
+
+                MaxSynapsesPerSegment = (int)(0.01 * numColumns),
+                Random = new ThreadSafeRandom(42),
+                StimulusThreshold = 10,
+            };
+
+            double max = 150;
+
+            //
+            // This dictionary defines a set of typical encoder parameters.
+            Dictionary<string, object> settings = new Dictionary<string, object>()
+            {
+                { "W", 15},
+                { "N", inputBits},
+                { "Radius", -1.0},
+                { "MinVal", 0.0},
+                { "Periodic", false},
+                { "Name", "scalar"},
+                { "ClipInput", false},
+                { "MaxVal", max}
+            };
+
+
+            EncoderBase encoder = new NeoCortexApi.Encoders.ScalarEncoder(settings);
+
+            //
+            // We create here 100 random input values.
+            List<double> inputValues = new List<double>();
+
+            for (int i = 0; i < (int)max; i++)
+            {
+                inputValues.Add((double)i);
+            }
+
+            var sp = RunExperiment(cfg, encoder, inputValues);
+
+          
+        }
         /// <summary>
         /// Implements the experiment.
         /// </summary>
