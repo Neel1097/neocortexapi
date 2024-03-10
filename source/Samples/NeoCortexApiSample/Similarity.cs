429330f808e0167a2682329898eb9924e3e5ec3c
Neel1097 <maityamit2017@gmail.com>
2024-03-10 18:02:12 +0100

    Correcting the second image path

--- source/Samples/NeoCortexApiSample/Similarity.cs
+++ source/Samples/NeoCortexApiSample/Similarity.cs
@@ -8,7 +8,7 @@ class BitmapSimilarity
     {
         // Replace these file paths with the paths to your bitmap images
         string imagePath1 = "F:\\Software desktop\\Team-ByteBaite_neocortexapi\\source\\Samples\\NeoCortexApiSample\\bin\\Debug\\net8.0\\outputFolder\\0.png";
-        string imagePath2 = "F:\\Software desktop\\Team-ByteBaite_neocortexapi\\source\\Samples\\NeoCortexApiSample\\bin\\Debug\\net8.0\\outputFolder\\0-output.png";
+        string imagePath2 = "F:\\Software desktop\\Team-ByteBaite_neocortexapi\\source\\Samples\\NeoCortexApiSample\\bin\\Debug\\net8.0\\outputFolder\\0--output.png";
 
         Bitmap bitmap1 = new Bitmap(imagePath1);
         Bitmap bitmap2 = new Bitmap(imagePath2);
