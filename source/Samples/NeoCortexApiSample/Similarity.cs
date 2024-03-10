131bcbf8aef7cd9b839d4ada95bd66d60426db50
Neel1097 <maityamit2017@gmail.com>
2024-03-10 17:41:05 +0100

    Adding the Similarity Class
    This class helps to find the similarity of the two bit map

---
+++ source/Samples/NeoCortexApiSample/Similarity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+using ScottPlot.Drawing.Colormaps;
+
+class BitmapSimilarity
+{
+    static void Main()
+    {
+        // Replace these file paths with the paths to your bitmap images
+        string imagePath1 = "F:\\Software desktop\\Team-ByteBaite_neocortexapi\\source\\Samples\\NeoCortexApiSample\\bin\\Debug\\net8.0\\outputFolder\\0.png";
+        string imagePath2 = "F:\\Software desktop\\Team-ByteBaite_neocortexapi\\source\\Samples\\NeoCortexApiSample\\bin\\Debug\\net8.0\\outputFolder\\0-output.png";
+
+        Bitmap bitmap1 = new Bitmap(imagePath1);
+        Bitmap bitmap2 = new Bitmap(imagePath2);
+    }
+
+}
