f982d5bf912d3fe41287ac998d8ec661341ca133
Neel1097 <maityamit2017@gmail.com>
2024-03-12 19:36:16 +0100

    Updating the Similarity class

--- source/Samples/NeoCortexApiSample/Similarity.cs
+++ source/Samples/NeoCortexApiSample/Similarity.cs
@@ -1,35 +1,16 @@
-﻿using System;
-
-
-class BitmapSimilarity
+﻿class Similarity
 {
-    static void Main()
+    public static double CalculateSim(int[] inpSdr, int[] thresholdvalues)
     {
-        //Calculating the similairity by using the two array
-
-        if (twoDimArray.GetLength(0) != twoDArray.GetLength(0) || twoDimArray.GetLength(1) != twoDArray.GetLength(1))
+        int matchingCount = 0;
+        for (int i = 0; i < inpSdr.Length; i++)
         {
-            throw new ArgumentException("Arrays must have the same dimensions.");
-        }
-        int totalElements = twoDimArray.GetLength(0) * twoDimArray.GetLength(1);
-        int matchingElements = 0;
-
-        for (int i = 0; i < twoDimArray.GetLength(0); i++)
-        {
-            for (int j = 0; j < twoDimArray.GetLength(1); j++)
+            if (inpSdr[i] == thresholdvalues[i])
             {
-                if (twoDimArray[i, j] == twoDArray[i, j])
-                {
-                    matchingElements++;
-                }
+                matchingCount++;
             }
         }
-
-        double similarity = matchingElements / totalElements;
-        Console.WriteLine(similarity);
+        var similarity = (double)matchingCount / inpSdr.Length * 100;
+        return similarity;
     }
-
-}    
 }
-
-
