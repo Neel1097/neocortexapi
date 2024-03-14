99d6f45686519798c76ab50ebe15994e4c80b3f9
Subham Singh <subham.reek@gmail.com>
2024-03-14 11:03:14 +0100

    Updated the similarity Code to align
    Updated the similarity code to align with the rest of the code and the class structure.

--- source/Samples/NeoCortexApiSample/Similarity.cs
+++ source/Samples/NeoCortexApiSample/Similarity.cs
@@ -1,7 +1,15 @@
-﻿class Similarity
+﻿using System;
+
+class Similarity
 {
     public static double CalculateSim(int[] inpSdr, int[] thresholdvalues)
     {
+        // Check if arrays have the same length
+        if (inpSdr.Length != thresholdvalues.Length)
+        {
+            throw new ArgumentException("Arrays must have the same length.");
+        }
+
         int matchingCount = 0;
         for (int i = 0; i < inpSdr.Length; i++)
         {
@@ -10,6 +18,7 @@
                 matchingCount++;
             }
         }
+
         var similarity = (double)matchingCount / inpSdr.Length * 100;
         return similarity;
     }
