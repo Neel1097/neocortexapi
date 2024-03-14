4244dd6d54c965b5412299656e917dfa414e70ff
Neel1097 <maityamit2017@gmail.com>
2024-03-15 00:30:18 +0100

    Adding the image binarizer

---
+++ source/Samples/NeoCortexApiSample/Image Binarizer.cs
@@ -0,0 +1,23 @@
+﻿using NeoCortex;
+using System;
+using Daenet.ImageBinarizerLib;
+using Daenet.ImageBinarizerLib.Entities;
+
+namespace Image_Binarizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imagePath = "";
+            string outputPath = ""; // Specify your desired output path
+            int threshold = 130; // Set the binarization threshold
+
+            NeoCortexUtils.BinarizeImage(imagePath, outputPath, threshold, "");
+
+            string textFilePath = ""; // Path to the text file
+
+            // Process the binary array as needed
+        }
+    }
+}
