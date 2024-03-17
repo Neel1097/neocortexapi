cdb21e98fe35aa082505320037badac155132c0b
Neel1097 <maityamit2017@gmail.com>
2024-03-17 11:22:56 +0100

    Creating a class for Jaccard Index Calculation
    Method to calculate the Jaccard index between two SDRs

---
+++ source/Samples/NeoCortexApiSample/JaccardIndexCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class JaccardIndexCalculator
+{
+    // Method to calculate the Jaccard index between two SDRs
+    public double CalculateJaccardIndex(int[] sdr1, int[] sdr2)
+    {
+        if (sdr1.Length != sdr2.Length)
+        {
+            throw new ArgumentException("SDRs must have the same length.");
+        }
+        return;
+    }
+}
