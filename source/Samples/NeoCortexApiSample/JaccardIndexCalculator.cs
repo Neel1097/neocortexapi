a3e49173d201cb990c102759ec5a01ddfd3cc2fe
Neel1097 <maityamit2017@gmail.com>
2024-03-17 13:03:52 +0100

    Updating the  JaccardIndexCalculator Class

--- source/Samples/NeoCortexApiSample/JaccardIndexCalculator.cs
+++ source/Samples/NeoCortexApiSample/JaccardIndexCalculator.cs
@@ -11,9 +11,26 @@ public class JaccardIndexCalculator
         {
             throw new ArgumentException("SDRs must have the same length.");
         }
+
         // Calculate intersection and union counts
         int intersection = 0;
         int union = 0;
 
+        for (int i = 0; i < sdr1.Length; i++)
+        {
+            if (sdr1[i] == 1 && sdr2[i] == 1)
+            {
+                intersection++;
+            }
+
+            if (sdr1[i] == 1 || sdr2[i] == 1)
+            {
+                union++;
+            }
+        }
+
+        // Calculate Jaccard index
+        double jaccardIndex = (double)intersection / union;
+        return jaccardIndex;
     }
 }
