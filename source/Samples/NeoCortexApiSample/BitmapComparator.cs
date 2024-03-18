afc2da3f6f9bc02f6326f31a49b215ce63eccc0f
Rubi Kiran <149596667+RubiKirann@users.noreply.github.com>
2024-03-18 16:21:22 +0100

    Updataing BitmapComparator class
    Analyzing the pixels in the two bitmaps and comparing their corresponding values.

--- source/Samples/NeoCortexApiSample/BitmapComparator.cs
+++ source/Samples/NeoCortexApiSample/BitmapComparator.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,24 @@ namespace NeoCortexApiSample
 {
     internal class BitmapComparator
     {
-    }
+        int matchingPixels = 0;
+        int totalPixels = bmp1.Width * bmp1.Height;
+
+        // Compare corresponding pixels in the two bitmaps
+        for (int x = 0; x<bmp1.Width; x++)
+        {
+            for (int y = 0; y<bmp1.Height; y++)
+            {
+                Color color1 = bmp1.GetPixel(x, y);
+        Color color2 = bmp2.GetPixel(x, y);
+
+                // Check if the colors are identical
+                if (color1.ToArgb() == color2.ToArgb())
+                {
+                    matchingPixels++;
+                }
 }
+
+
+
+   
