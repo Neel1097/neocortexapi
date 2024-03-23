7e9c8ba10b0bfcdfba970355717ce52fc562fc78
Subham Singh <subham.reek@gmail.com>
2024-03-23 19:10:48 +0100

    Update ImgBinarizer class
    Converted the type from int to double to align with the Spatial Pattern learning code.

--- source/Samples/NeoCortexApiSample/ImgBinarizer.cs
+++ source/Samples/NeoCortexApiSample/ImgBinarizer.cs
@@ -3,16 +3,14 @@ using Daenet.ImageBinarizerLib.Entities;
 
 public class ImgBinarizer
 {
-    public static double[] Imgo()
+    public static int[] Imgo()
     {
         var parameters = new BinarizerParams
         {
-            InputImagePath = "F:\\YOURDIRR\\Picture\\MNIST_6_0.png",
-            ImageHeight = 238,
-            ImageWidth = 238,
-            //BlueThreshold = 200,
-            //RedThreshold = 200,
-            //GreenThreshold = 200
+            InputImagePath = "C:\\Users\\ACER\\OneDrive\\Desktop\\2.png",
+            ImageHeight = 28,
+            ImageWidth = 28,
+            
         };
         //string imagePath = "F:\\YOURDIRR\\Picture\\MNIST_6_0.png";
         //string outputPath = "F:\\YOURDIRR\\Picture"; //Specify your desired output path
@@ -21,12 +19,12 @@ public class ImgBinarizer
         var doubleArray = bizer.GetArrayBinary();
         var hg = doubleArray.GetLength(1);
         var wd = doubleArray.GetLength(0);
-        var intArray = new double[hg * wd];
+        var intArray = new int[hg * wd];
         for (int j = 0; j < hg; j++)
         {
             for (int i = 0; i < wd; i++)
             {
-                intArray[j * wd + i] = (double)doubleArray[i, j, 0];
+                intArray[j * wd + i] = (int)doubleArray[i, j, 0];
             }
         }
         return intArray;
